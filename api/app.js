3ac3f888658679f60b36eca91adc638ed10b2d27
xuejuhui <xuejuhui@hotmail.com>
2021-04-18 12:23:06 -0400

    react build file

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const db = require("./models/index");
 
 const urlRoute = require("./routes/url");
@@ -13,10 +14,16 @@ db.sequelize
   })
   .catch((err) => console.log(err));
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", urlRoute);
 app.use("/api", groupRoute);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
